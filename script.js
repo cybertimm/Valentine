000f45b662fd5e1a4a27189d67703a24bc3b02f0
Ayeni Oluwadamilola Timothy <41154072+cybertimm@users.noreply.github.com>
2026-01-13 11:44:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yes");
+const msg = document.getElementById("message");
 
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * window.innerWidth * 0.7;
+  const y = Math.random() * window.innerHeight * 0.7;
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+  msg.innerHTML = "YAY!!! I knew it 💕🥰";
+});
