ab2a1c4b7873a86cfa47ec52ed6f23692c304667
Ayeni Oluwadamilola Timothy <41154072+cybertimm@users.noreply.github.com>
2026-01-13 12:00:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,35 @@ document.getElementById("yes").addEventListener("click", () => {
     "YAY!!! 💕🥰 I knew you'd say yes!";
   startConfetti();
 });
+
+const canvas = document.getElementById("confetti");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let pieces = [];
+
+function startConfetti() {
+  for (let i = 0; i < 300; i++) {
+    pieces.push({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      r: Math.random() * 6 + 2,
+      d: Math.random() * 5 + 1,
+    });
+  }
+  draw();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  pieces.forEach(p => {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    p.y += p.d;
+    if (p.y > canvas.height) p.y = 0;
+  });
+  requestAnimationFrame(draw);
+}
